4acc02afe01269c3f5f58cf8ed037ef502b7b00d
Ivan <ivansannikov29@gmail.com>
2025-11-26 01:25:57 +0300

    Working version with minimal configuration
    Tramp is working fast. Basicaly no modeline. Git is through vc-mode

--- init.el
+++ init.el
@@ -1,11 +1,17 @@
 ;; startup
-(add-to-list 'custom-theme-load-path "gruber-darker-theme.el")
-(add-to-list 'default-frame-alist `(font . "IosevkaFixed Nerd Font Extended 14"))
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(setq custom-file "custom.el")
+(load custom-file)
+(add-to-list 'default-frame-alist
+			 `(font . "IosevkaFixed Nerd Font Extended 13"))
 (add-to-list 'default-frame-alist `(fullscreen . maximized))
+(load-theme 'gruber-darker t)
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
+(savehist-mode 1)
 
 ;; globals
 (setq inhibit-splash-screen t)
@@ -23,16 +29,83 @@
 (when (eq system-type 'windows-nt)
   (setq tramp-default-method "plink"))
 (setq remote-file-name-inhibit-locks t
-      tramp-use-scp-direct-remote-copying t
-      remote-file-name-inhibit-auto-save-visited t)
+	  tramp-use-scp-direct-remote-copying t
+	  remote-file-name-inhibit-auto-save-visited t)
 
+;; completions
+(icomplete-vertical-mode 1)
+(keymap-set icomplete-minibuffer-map "TAB" #'icomplete-force-complete)
+(setq completion-category-overrides
+   '((buffer
+	  (styles . (basic flex))
+	  (cycle  . 10))
+	 (file
+	  (styles . (initials flex))
+	  (cycle  . 10))
+	 (symbol-help
+	  (styles basic shorthand substring))))
 (setq completion-auto-select nil)
 (setq completion-show-help nil)
 (setq completion-auto-help nil)
 (setq completions-sort 'historical)
+(setq icomplete-show-matches-on-no-input t)
+(setq minibuffer-completion-auto-choose nil)
+(add-to-list 'completion-ignored-extensions "./")
+(add-to-list 'completion-ignored-extensions "../")
+
 (setq dired-listing-switches "-Alh")
 (setq font-lock-maximum-deciration 2)
 (setq treesit-font-lock-level 2)
+(setq imenu-flatten 'annotation)
 
-(setq custom-file "custom.el")
-(load custom-file)
+;;modeline
+(setq-default mode-line-format '("%e" mode-line-front-space
+								 (:propertize
+								  ("" mode-line-mule-info mode-line-client
+								   mode-line-modified
+								   mode-line-remote mode-line-window-dedicated)
+								  display (min-width (6.0)))
+								 mode-line-frame-identification
+								 mode-line-buffer-identification
+								 "  "
+								 (project-mode-line project-mode-line-format)
+								 (vc-mode vc-mode)
+								 "	" 
+								 mode-name
+								 mode-line-process
+								 mode-line-end-spaces))
+
+(defun up-directory (arg)
+  "Move up a directory (delete backwards to /)."
+  (interactive "p")
+  (if (string-match-p "/." (minibuffer-contents))
+	  (zap-up-to-char (- arg) ?/)
+	(delete-minibuffer-contents)))
+
+;; c-ts-mode
+(require 'c-ts-mode)
+(setq major-mode-remap-alist '((c-mode . c-ts-mode) (c++-mode . c++-ts-mode)))
+(add-hook 'after-change-major-mode-hook (lambda () (electric-indent-mode -1)))
+(setq-default c-ts-mode-indent-offset 4)
+(setq-default c-ts-mode-indent-style 'bsd)
+(add-hook 'c-ts-mode-hook   (lambda () (setq-local indent-tabs-mode nil)))
+(add-hook 'c++-ts-mode-hook (lambda () (setq-local indent-tabs-mode nil)))
+
+;; c-mode
+(setq-default c-basic-offset 4)
+(setq-default c-default-style "bsd")
+(add-hook 'c-mode-hook   (lambda () (setq-local indent-tabs-mode nil)))
+(add-hook 'c++-mode-hook (lambda () (setq-local indent-tabs-mode nil)))
+
+;; buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'nil)
+
+(setq isearch-repeat-on-direction-change t)
+
+;; keybindings
+(keymap-set minibuffer-local-filename-completion-map
+			"C-l" #'up-directory)
+(global-set-key (kbd "C-x C-/") 'goto-last-change)
+
+(load "packages.el")
---
+++ lisp/packages.el
@@ -0,0 +1,63 @@
+;; packages
+(require 'package)
+(add-to-list 'package-archives '("MELPA" . "http://melpa.org/packages/"))
+(setq package-install-upgrade-built-in t)
+(require 'use-package)
+
+;; markdown-mode
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "pandoc"))
+
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  (("C-S-c C-S-c" . 'mc/edit-lines)
+   ("C->"         . 'mc/mark-next-like-this)
+   ("C-<"         . 'mc/mark-previous-like-this)
+   ("C-c C-<"     . 'mc/mark-all-like-this)
+   ("C-\""        . 'mc/skip-to-next-like-this)
+   ("C-:"         . 'mc/skip-to-previous-like-this)))
+
+(use-package corfu
+  :ensure t
+  :config
+  (global-corfu-mode))
+
+(use-package org
+  :init
+  (setq org-startup-folded t)
+  (setq org-startup-truncated nil)
+  :config
+  (global-set-key (kbd "C-c l") #'org-store-link)
+  (global-set-key (kbd "C-c a") #'org-agenda)
+  (global-set-key (kbd "C-c c") #'org-capture)
+  (global-set-key (kbd "C-c C") #'org-capture-goto-last-stored))
+
+(use-package dabbrev
+  :bind (("C-M-/"   . #'cape-dabbrev)
+         ("M-/" . dabbrev-expand))
+  :init
+  (setq dabbrev-case-fold-search nil)
+  :config
+  (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
+  (add-to-list 'dabbrev-ignored-buffer-modes 'authinfo-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode)
+  (add-to-list 'dabbrev-ignored-buffer-modes 'tags-table-mode))
+
+(use-package cape
+  :ensure t
+  :bind ("C-c p" . cape-prefix-map)
+  :init
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block))
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package move-text
+  :ensure t)
+
